9fa0180f22a1e0a870d18485a918a15fc5f3a850
giselMontano <giselalesita@gmail.com>
2022-05-31 21:35:57 -0400

    1st commit

---
+++ src/gitReview1/review001.java
@@ -0,0 +1,11 @@
+package gitReview1;
+
+public class review001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("learning");
+System.out.println("I can do it");
+	}
+
+}
